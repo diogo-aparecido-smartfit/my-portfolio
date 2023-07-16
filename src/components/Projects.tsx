2ba1b4196b398a4c498a4b84ae59e1512db1f82f
DiogoAMV <diogo.amv19@gmail.com>
2023-07-15 22:23:30 -0300

    see all projects button disabled

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import { LanguageContext } from "../App";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ProjectCard from "./ProjectCard";
 import {
   BiLogoTypescript,
@@ -7,10 +7,51 @@ import {
   BiLogoReact,
 } from "react-icons/bi";
 import { SiDotnet } from "react-icons/si";
+import ProjectsModal from "./ProjectsModal";
 
-const projects = [
+export const projects = [
   {
     id: 1,
+    portugueseDate: "2023 - present",
+    portugueseName: "Nome do projeto",
+    portugueseDescription:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    portugueseView: "Visualizar",
+    date: "2023 - present",
+    name: "Name of project",
+    description:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    view: "View",
+    deploy: "https://google.com",
+    technologies: [
+      <BiLogoTypescript />,
+      <BiLogoJavascript />,
+      <BiLogoReact />,
+      <SiDotnet />,
+    ],
+  },
+  {
+    id: 2,
+    portugueseDate: "2023 - present",
+    portugueseName: "Nome do projeto",
+    portugueseDescription:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    portugueseView: "Visualizar",
+    date: "2023 - present",
+    name: "Name of project",
+    description:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    view: "View",
+    deploy: "https://google.com",
+    technologies: [
+      <BiLogoTypescript />,
+      <BiLogoJavascript />,
+      <BiLogoReact />,
+      <SiDotnet />,
+    ],
+  },
+  {
+    id: 3,
     portugueseDate: "2023 - present",
     portugueseName: "Nome do projeto",
     portugueseDescription:
@@ -32,6 +73,7 @@ const projects = [
 ];
 
 export default function Projects() {
+  const [showModal, setShowModal] = useState(false);
   const language = useContext(LanguageContext);
 
   const data = [
@@ -63,10 +105,14 @@ export default function Projects() {
         ))}
       </div>
 
-      <a className="group/edit flex flex-row font-medium text-base max-w-fit hover:brightness-50 transition-all gap-2">
+      <button
+        // onClick={() => setShowModal(true)}
+        className="group/edit flex flex-row font-medium text-base max-w-fit hover:brightness-50 transition-all gap-2 cursor-not-allowed brightness-50"
+      >
         {language === "pt-BR" ? data[0].footer : data[1].footer}
         <span className="group-hover/edit:translate-x-1 transition-all">→</span>
-      </a>
+      </button>
+      {showModal ? <ProjectsModal setShowModal={setShowModal} /> : null}
     </section>
   );
 }
