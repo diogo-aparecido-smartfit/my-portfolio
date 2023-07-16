6c882f8a89894f73324a463da3bd2b211d5d48b2
DiogoAMV <diogo.amv19@gmail.com>
2023-07-15 21:18:51 -0300

    react router dom configurated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./global.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import ProjectsPage from "./pages/ProjectsPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "projects",
+    element: <ProjectsPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
