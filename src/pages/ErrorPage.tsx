7c0d17a6e3d2a74b1b4b56315c3ab730ba6d89d9
DiogoAMV <diogo.amv19@gmail.com>
2023-07-15 22:04:49 -0300

    not found page configurated

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,41 +1,25 @@
-import { useContext } from "react";
 import { Link, useRouteError } from "react-router-dom";
-import { LanguageContext } from "../App";
 
 export default function ErrorPage() {
-  const data = [
-    {
-      message: "Voltar para o início",
-    },
-    {
-      message: "Back to home",
-    },
-  ];
-
-  const language = useContext(LanguageContext);
-
   const error: any = useRouteError();
   console.error(error);
 
   return (
-    <>
-      <section
-        id="home"
-        className="flex flex-col h-screen items-center justify-center text-left gap-1"
-      >
-        <h1 className="font-medium text-lg">
-          Ops... {error.statusText || error.message}
-        </h1>
-
-        <Link to={`/`}>
-          <a className="group/edit flex flex-row font-medium text-base max-w-fit hover:brightness-50 transition-all gap-2">
-            {language === "pt-BR" ? data[0].message : data[1].message}
-            <span className="group-hover/edit:translate-x-1 transition-all">
-              →
-            </span>
-          </a>
-        </Link>
-      </section>
-    </>
+    <section
+      className="flex flex-col items-center justify-center h-screen text-center gap-2 p-4"
+      id="contact"
+    >
+      <h1>
+        Ops... <i>{error.statusText || error.message}</i>
+      </h1>
+      <Link to={`/`}>
+        <a className="group/edit flex flex-row font-medium text-base max-w-fit hover:brightness-50 transition-all gap-2">
+          Back to home
+          <span className="group-hover/edit:translate-x-1 transition-all">
+            →
+          </span>
+        </a>
+      </Link>
+    </section>
   );
 }
