a9ac5b4b6cb11ce59e2f5f54a9fe1953daaa2b08
DiogoAMV <diogo.amv19@gmail.com>
2023-07-18 15:40:25 -0300

    Projects List created

---
+++ src/data/projects.tsx
@@ -0,0 +1,69 @@
+import {
+  BiLogoTypescript,
+  BiLogoJavascript,
+  BiLogoReact,
+} from "react-icons/bi";
+import { SiDotnet } from "react-icons/si";
+
+export const projects = [
+  {
+    id: 1,
+    portugueseDate: "2023 - present",
+    portugueseName: "Nome do projeto",
+    portugueseDescription:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    portugueseView: "Visualizar",
+    date: "2023 - present",
+    name: "Name of project",
+    description:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    view: "View",
+    deploy: "https://google.com",
+    technologies: [
+      <BiLogoTypescript />,
+      <BiLogoJavascript />,
+      <BiLogoReact />,
+      <SiDotnet />,
+    ],
+  },
+  {
+    id: 2,
+    portugueseDate: "2023 - present",
+    portugueseName: "Nome do projeto",
+    portugueseDescription:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    portugueseView: "Visualizar",
+    date: "2023 - present",
+    name: "Name of project",
+    description:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    view: "View",
+    deploy: "https://google.com",
+    technologies: [
+      <BiLogoTypescript />,
+      <BiLogoJavascript />,
+      <BiLogoReact />,
+      <SiDotnet />,
+    ],
+  },
+  {
+    id: 3,
+    portugueseDate: "2023 - present",
+    portugueseName: "Nome do projeto",
+    portugueseDescription:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    portugueseView: "Visualizar",
+    date: "2023 - present",
+    name: "Name of project",
+    description:
+      "Fill your project brief here. It can be the outcome of the project, or some success metrics, or a cheesy tagline.",
+    view: "View",
+    deploy: "https://google.com",
+    technologies: [
+      <BiLogoTypescript />,
+      <BiLogoJavascript />,
+      <BiLogoReact />,
+      <SiDotnet />,
+    ],
+  },
+];
