647b1435b8d10a67494745b49b6620c8f51d2d24
DiogoAMV <diogo.amv19@gmail.com>
2023-08-03 21:50:54 -0300

    mongodb configurated

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    const port = 3001;
+    const app = express();
 
-app.listen(3001, () => {
-  console.log("🔥 Server is running on http://localhost:3001");
-});
+    app.listen(port, () => {
+      console.log(`🔥 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log("erro ao conectar no mongodb"));
