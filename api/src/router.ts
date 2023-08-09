c8da36957dd22e26fb938502b21721071dd0b82c
DiogoAMV <diogo.amv19@gmail.com>
2023-08-09 16:37:30 -0300

    all routes created

--- api/src/router.ts
+++ api/src/router.ts
@@ -8,9 +8,21 @@ router.get("/projects", (req, res) => {
 });
 
 // Create project
+router.post("/projects", (req, res) => {
+  res.send("OK");
+});
 
 // Update project
+router.put("/projects/:id", (req, res) => {
+  res.send("OK");
+});
 
 // Delete project
+router.get("/projects/:id", (req, res) => {
+  res.send("OK");
+});
 
 // List projects by category
+router.get("/categories/:categoryId/products", (req, res) => {
+  res.send("OK");
+});
