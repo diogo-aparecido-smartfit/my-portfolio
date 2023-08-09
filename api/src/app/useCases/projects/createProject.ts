f2eaedb7a467c528d47e40c076cb905088fc9ffa
DiogoAMV <diogo.amv19@gmail.com>
2023-08-09 17:15:55 -0300

    case createProject configurated

---
+++ api/src/app/useCases/projects/createProject.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+
+import { Project } from "../../models/Project";
+
+export async function createProject(req: Request, res: Response) {
+  try {
+    const {
+      date,
+      name,
+      description,
+      imagePath,
+      technologies,
+      deployLink,
+      githubLink,
+      category,
+    } = req.body;
+
+    const project = await Project.create({
+      date,
+      name,
+      description,
+      imagePath,
+      technologies,
+      deployLink,
+      githubLink,
+      category,
+    });
+
+    res.json(project);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
