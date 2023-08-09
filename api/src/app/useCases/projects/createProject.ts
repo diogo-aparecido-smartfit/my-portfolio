f295ae2932daaa6333de5c4142fb7e7569ff1d2a
DiogoAMV <diogo.amv19@gmail.com>
2023-08-09 17:46:10 -0300

    createProject case changed

--- api/src/app/useCases/projects/createProject.ts
+++ api/src/app/useCases/projects/createProject.ts
@@ -4,29 +4,54 @@ import { Project } from "../../models/Project";
 
 export async function createProject(req: Request, res: Response) {
   try {
+    // const {
+    //   date,
+    //   name,
+    //   description,
+    //   imagePath,
+    //   technologies,
+    //   deployLink,
+    //   githubLink,
+    //   category,
+    // } = req.body;
+
+    // const project = await Project.create({
+    //   date,
+    //   name,
+    //   description,
+    //   imagePath,
+    //   technologies,
+    //   deployLink,
+    //   githubLink,
+    //   category,
+    // });
+
+    // res.status(201).json(project);
+
+    const image = req.file?.filename;
+
     const {
       date,
       name,
       description,
-      imagePath,
       technologies,
       deployLink,
       githubLink,
       category,
     } = req.body;
 
-    const project = await Project.create({
+    const product = await Project.create({
       date,
       name,
       description,
-      imagePath,
+      image,
       technologies,
       deployLink,
       githubLink,
       category,
     });
 
-    res.status(201).json(project);
+    res.status(201).json(product);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
