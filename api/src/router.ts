fc63952200fad69cb469118b41a32b0271075d0e
DiogoAMV <diogo.amv19@gmail.com>
2023-08-09 17:46:37 -0300

    multer configured

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,14 +1,27 @@
 import { Router } from "express";
+import path from "node:path";
 import { listProjects } from "./app/useCases/projects/listProjects";
 import { createProject } from "./app/useCases/projects/createProject";
+import multer from "multer";
 
 export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, "..", "uploads"));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    },
+  }),
+});
 
 // List projects
 router.get("/projects", listProjects);
 
 // Create project
-router.post("/projects", createProject);
+router.post("/projects", upload.single("image"), createProject);
 
 // Update project
 router.put("/projects/:id", (req, res) => {
