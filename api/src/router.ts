03aebae798486531799641eecc03df8c957d1a4a
DiogoAMV <diogo.amv19@gmail.com>
2023-08-09 16:30:01 -0300

    list projects route created

---
+++ api/src/router.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+export const router = Router();
+
+// List projects
+router.get("/projects", (req, res) => {
+  res.send("OK");
+});
+
+// Create project
+
+// Update project
+
+// Delete project
+
+// List projects by category
