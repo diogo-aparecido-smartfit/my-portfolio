65a10a18167f7034db962ea1e12ad38f601e5705
DiogoAMV <diogo.amv19@gmail.com>
2023-08-11 18:28:52 -0300

    upload image to firebase configurated

--- api/src/app/useCases/projects/createProject.ts
+++ api/src/app/useCases/projects/createProject.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
-
 import { Project } from "../../models/Project";
+import { bucket } from "../../firebaseConfig";
 
 export async function createProject(req: Request, res: Response) {
   try {
-    const image = req.file?.filename;
+    const imageFile = req.file;
+
+    if (!imageFile) {
+      return res.status(400).send("No file uploaded.");
+    }
 
     const technologiesInput = req.body.technologies;
     const technologiesArray = technologiesInput
@@ -21,11 +25,25 @@ export async function createProject(req: Request, res: Response) {
       category,
     } = req.body;
 
+    // Faz o upload da imagem no Firebase Cloud Storage
+    const filePath = `uploads/${Date.now()}-${imageFile.originalname}`;
+    const bucketFile = bucket.file(filePath);
+
+    await bucketFile.save(imageFile.buffer, {
+      metadata: {
+        contentType: imageFile.mimetype,
+      },
+    });
+
+    const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+      bucket.name
+    }/o/${encodeURIComponent(filePath)}?alt=media`;
+
     const product = await Project.create({
       date,
       name,
       description,
-      image,
+      image: imageUrl,
       technologies: technologiesArray,
       deployLink,
       githubLink,
