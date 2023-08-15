231f45db8340e591be3d2c19894e895b1d4ea7b4
DiogoAMV <diogo.amv19@gmail.com>
2023-08-15 11:56:01 -0300

    new icon added

--- web/src/pages/ErrorPage.tsx
+++ web/src/pages/ErrorPage.tsx
@@ -1,8 +1,14 @@
+import Lottie from "lottie-react";
+import { useRef } from "react";
 import { Link, useRouteError } from "react-router-dom";
+import notFoundIcon from "../../public/icons/404-icon.json";
+import errorIcon from "../../public/icons/error-icon.json";
 
 export default function ErrorPage() {
   const error: any = useRouteError();
   console.error(error);
+  const errorRef = useRef(null);
+  const notFoundRef = useRef(null);
 
   return (
     <section
@@ -12,6 +18,23 @@ export default function ErrorPage() {
       <h1>
         Ops... <i>{error.statusText || error.message}</i>
       </h1>
+      {error.statusText === "Not Found" ? (
+        <Lottie
+          lottieRef={notFoundRef}
+          className="w-48 h-48"
+          animationData={notFoundIcon}
+          loop={true}
+          autoplay={true}
+        />
+      ) : (
+        <Lottie
+          lottieRef={errorRef}
+          className="w-48 h-48"
+          animationData={errorIcon}
+          loop={true}
+          autoplay={true}
+        />
+      )}
       <Link to={`/`}>
         <a className="group/edit flex flex-row font-medium text-base max-w-fit hover:brightness-50 transition-all gap-2">
           Back to home
