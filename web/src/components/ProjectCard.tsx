88289fc49d4d097a98d8d93a41a16c33eda39ee2
DiogoAMV <diogo.amv19@gmail.com>
2023-08-31 15:02:09 -0300

    project details modal created

--- web/src/components/ProjectCard.tsx
+++ web/src/components/ProjectCard.tsx
@@ -1,104 +1,317 @@
 import eyeIcon from "../../public/icons/eye-icon.json";
-import { FaReact, FaNodeJs, FaMobileAlt, FaJsSquare } from "react-icons/fa";
 import Lottie from "lottie-react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { PiPuzzlePieceFill } from "react-icons/pi";
+import {
+  IoIosArrowBack,
+  IoIosArrowDown,
+  IoIosArrowForward,
+  IoIosClose,
+} from "react-icons/io";
+import { IoShareOutline } from "react-icons/io5";
+import { HiOutlineBookOpen } from "react-icons/hi";
+import { FiCopy, FiGithub } from "react-icons/fi";
+import { BiSolidLockAlt } from "react-icons/bi";
+import safariIcon from "../../public/icons/safari-icon.svg";
+import { LiaArrowCircleDownSolid } from "react-icons/lia";
+import { AiOutlinePlus } from "react-icons/ai";
+import { TbWorldShare } from "react-icons/tb";
+
 interface ProjectCardProps {
   language: string;
   projects: {
     id: string;
-    portugueseDate: string;
-    portugueseName: string;
-    portugueseDescription: string;
-    portugueseView: string;
-    image: string;
     date: string;
+    enDate: string;
     name: string;
+    enName: string;
     description: string;
+    enDescription: string;
     view: string;
+    enView: string;
+    image: string;
+    github: string;
     deploy: string;
-    technologies: [];
+    technologies: JSX.Element[];
   };
 }
 
 export default function ProjectCard({ language, projects }: ProjectCardProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleVisualize = () => {
+    setIsModalOpen(true);
+  };
+
   const lottieRef = useRef(null);
 
-  function getTechnologyIcon(techName: string): React.ReactNode {
-    if (techName === "reactjs") {
-      return <FaReact />;
-    } else if (techName === "reactnative") {
-      return <FaMobileAlt />;
-    } else if (techName === "javascript") {
-      return <FaJsSquare />;
-    } else if (techName === "nodejs") {
-      return <FaNodeJs />;
-    } else {
-      return null;
-    }
-  }
-
   return (
-    <article className="flex flex-col md:flex-row gap-2">
-      <div className="flex flex-col gap-2 md:w-[380px]">
-        <header>
-          <p className="text-xs opacity-50">
-            {language === "pt-BR" ? projects.portugueseDate : projects.date}
+    <>
+      <article className="flex flex-col md:flex-row gap-2">
+        <div className="flex flex-col gap-2 md:w-[380px]">
+          <header>
+            <p className="text-xs opacity-50">
+              {language === "pt-BR" ? projects.date : projects.enDate}
+            </p>
+            <h1 className="font-medium text-base">
+              {language === "pt-BR" ? projects.name : projects.enName}
+            </h1>
+          </header>
+          <p className="font-normal text-base opacity-70">
+            {language === "pt-BR"
+              ? projects.description
+              : projects.enDescription}
           </p>
-          <h1 className="font-medium text-base">
-            {language === "pt-BR" ? projects.portugueseName : projects.name}
-          </h1>
-        </header>
-        <p className="font-normal text-base opacity-70">
-          {language === "pt-BR"
-            ? projects.portugueseDescription
-            : projects.description}
-        </p>
-        <footer className="flex flex-col gap-2">
-          <ul className="flex flex-row list-none text-xl gap-4 ">
-            {" "}
-            {projects.technologies.map((icon, index) => (
-              <li
-                className={`hover:scale-150 hover:text-zinc-500 opacity-70 hover:opacity-100 hover:cursor-pointer transition-all`}
-                key={index}
+          <footer className="flex flex-col gap-2">
+            <ul className="flex flex-row list-none text-xl gap-4 ">
+              {projects.technologies.map((icon, index) => (
+                <li
+                  className={`hover:scale-150 hover:text-zinc-500 opacity-70 hover:opacity-100 hover:cursor-pointer transition-all`}
+                  key={index}
+                >
+                  {icon}
+                </li>
+              ))}
+            </ul>
+            <div className="flex flex-row gap-3">
+              <button
+                className="group/edit hover:underline flex flex-row items-center gap-2 max-w-fit transition-all cursor-pointer"
+                onMouseEnter={() => (lottieRef as any)?.current?.play()}
+                onMouseLeave={() => (lottieRef as any)?.current?.stop()}
+                onClick={handleVisualize}
               >
-                {getTechnologyIcon(icon)}
-              </li>
-            ))}
-          </ul>
-          <a
-            className="group/edit hover:underline flex flex-row items-center gap-2 max-w-fit transition-all cursor-pointer"
-            target="_blank"
-            onMouseEnter={() => (lottieRef as any)?.current?.play()}
-            onMouseLeave={() => (lottieRef as any)?.current?.stop()}
-            href={projects.deploy}
-          >
-            {language === "pt-BR" ? "Visualizar" : "View"}
-            <span
-              className={`${
-                localStorage.getItem("darkMode") === "true"
-                  ? "brightness-200" // Classe de estilo para o modo escuro
-                  : "brightness-50" // Classe de estilo para o modo claro
-              }`}
-            >
-              <Lottie
-                lottieRef={lottieRef}
-                className="w-6 h-6"
-                animationData={eyeIcon}
-                loop={false}
-                autoplay={false}
-              />
-            </span>
-          </a>
-        </footer>
-      </div>
+                {language === "pt-BR" ? "Mais detalhes" : "More details"}
+                <span
+                  className={`${
+                    localStorage.getItem("darkMode") === "true"
+                      ? "brightness-150"
+                      : "brightness-50"
+                  }`}
+                >
+                  <Lottie
+                    lottieRef={lottieRef}
+                    className="w-6 h-6"
+                    animationData={eyeIcon}
+                    loop={false}
+                    autoplay={false}
+                  />
+                </span>
+              </button>
+            </div>
+          </footer>
+        </div>
 
-      <div className="flex flex-col relative">
-        <img
-          className="w-[145px] h-[145px] rounded-2xl relative"
-          src={projects.image}
-          alt="Imagem do projeto"
-        />
-      </div>
-    </article>
+        <div className="flex flex-col relative">
+          <img
+            className="w-[145px] h-[145px] rounded-2xl relative"
+            src={projects.image}
+            alt="Imagem do projeto"
+          />
+        </div>
+      </article>
+
+      {isModalOpen ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none shadow-2xl bg-zinc-900/90">
+            <div className="relative mx-2 lg:scale-100 w-auto my-6 max-w-7xlxl ">
+              {/*content*/}
+              <div
+                className={`border-0 rounded-2xl shadow-lg relative flex flex-col w-full outline-none focus:outline-none ${
+                  localStorage.getItem("darkMode") === "true"
+                    ? "bg-[#464646]"
+                    : "bg-[#f8f8f8]"
+                }`}
+              >
+                {/*header*/}
+                <header
+                  className={`hidden md:flex items-center justify-around h-[52px] rounded-t-xl ${
+                    localStorage.getItem("darkMode") === "true"
+                      ? "bg-[#363636]"
+                      : "bg-white"
+                  }`}
+                >
+                  <div
+                    className={`flex absolute w-full items-center justify-center  ${
+                      localStorage.getItem("darkMode") === "true"
+                        ? "bg-[#F1F1F133] text-[#F1F1F1]"
+                        : "bg-black/10 text-[#3D3D3D]"
+                    } max-w-md rounded-md  h-[28px]`}
+                  >
+                    <h1 className="flex items-center gap-1 text-sm font-normal ">
+                      {language === "pt-BR" ? projects.name : projects.enName}{" "}
+                      <BiSolidLockAlt />
+                    </h1>
+                  </div>
+                  <div className="flex w-full justify-between">
+                    <div className="flex gap-2 items-center ml-4 transition-all">
+                      <div className="flex gap-2">
+                        <button
+                          onClick={() => setIsModalOpen(false)}
+                          className="group/edit flex items-center justify-center bg-[#ED6A5F] rounded-full w-[12px] h-[12px] hover:brightness-50 transition-all"
+                        >
+                          <span className="hidden group-hover/edit:flex font-bold text-red-700">
+                            <IoIosClose />
+                          </span>
+                        </button>
+                        <button
+                          onClick={() => setIsModalOpen(false)}
+                          className="group/edit flex items-center justify-center bg-[#F5BF4F] rounded-full w-[12px] h-[12px] hover:brightness-50 transition-all"
+                        >
+                          <span className="hidden group-hover/edit:flex font-bold text-yellow-700">
+                            -
+                          </span>
+                        </button>
+                        <button className="group/edit flex items-center justify-center bg-[#61C554] rounded-full w-[12px] h-[12px] hover:brightness-50 transition-all"></button>
+                      </div>
+                      <div className="flex gap-1 ml-6 items-center">
+                        <img
+                          className={`hover:brightness-[.1] cursor-pointer transition-all ${
+                            localStorage.getItem("darkMode") === "true"
+                              ? "brightness-100"
+                              : "brightness-[.2]"
+                          }`}
+                          src={safariIcon}
+                          alt="icon"
+                        />
+                        <div className="w-[1px] h-[18px] bg-white/20 "></div>
+                        <span className="text-xs hover:brightness-50 cursor-pointer transition-all">
+                          <IoIosArrowDown />
+                        </span>
+                      </div>
+                      <div className="flex ml-1 transition-all hover:brightness-50 cursor-pointer">
+                        <IoIosArrowBack />
+                      </div>
+                    </div>
+
+                    <div className="flex gap-3 items-center ml-2 mr-4">
+                      <button className=" transition-all hover:brightness-50 cursor-pointer">
+                        <IoShareOutline />
+                      </button>
+                      <button className=" transition-all hover:brightness-50 cursor-pointer">
+                        <LiaArrowCircleDownSolid />
+                      </button>
+                      <button className=" transition-all hover:brightness-50 cursor-pointer">
+                        <AiOutlinePlus />
+                      </button>
+                    </div>
+                  </div>
+                </header>
+                {/*body*/}
+                <div
+                  className={`relative p-6 flex-auto max-h-[70vh] xl:max-h-[35rem]  overflow-y-auto rounded-t-xl md:rounded-t-none md:rounded-b-xl ${
+                    localStorage.getItem("darkMode") === "true"
+                      ? ""
+                      : "bg-[#f8f8f8]"
+                  }`}
+                >
+                  <header>
+                    <div className="flex max-w-sm">
+                      <img
+                        className="rounded-2xl"
+                        src="https://source.unsplash.com/random"
+                        alt="Imagem do projeto"
+                      />
+                    </div>
+                    <div className="mt-6">
+                      <p className="text-xs opacity-50">
+                        {language === "pt-BR" ? projects.date : projects.enDate}
+                      </p>
+                      <h1 className="font-medium text-lg">
+                        {language === "pt-BR" ? projects.name : projects.enName}
+                      </h1>
+                    </div>
+                  </header>
+                  <p className="my-4 text-base leading-relaxed">
+                    {language === "pt-BR"
+                      ? projects.description
+                      : projects.enDescription}
+                  </p>
+                  {/*footer*/}
+                  <footer className="flex flex-col gap-2">
+                    <ul className="flex flex-row list-none text-2xl gap-4 ">
+                      {projects.technologies.map((icon, index) => (
+                        <li
+                          className={`hover:scale-150 hover:text-zinc-500 opacity-70 hover:opacity-100 hover:cursor-pointer transition-all`}
+                          key={index}
+                        >
+                          {icon}
+                        </li>
+                      ))}
+                    </ul>
+                    <div className="flex flex-row gap-3 mt-4">
+                      <a
+                        className={`flex flex-row items-center gap-2 max-w-fit mt-4 text-base  transition-all ${
+                          projects.github === ""
+                            ? "cursor-not-allowed brightness-[.2]"
+                            : "hover:underline hover:brightness-50 cursor-pointer"
+                        }`}
+                        target="_blank"
+                        href={projects.github}
+                      >
+                        Github <FiGithub />
+                      </a>
+                      <a
+                        className={`flex flex-row items-center gap-2 max-w-fit  mt-4 text-base  transition-all ${
+                          projects.deploy === ""
+                            ? "cursor-not-allowed text-white/10"
+                            : "hover:underline hover:brightness-50 cursor-pointer"
+                        }`}
+                        target="_blank"
+                        href={projects.github}
+                      >
+                        Deploy <TbWorldShare />
+                      </a>
+                    </div>
+                  </footer>
+                </div>
+
+                {/* mobile header */}
+                <header className="md:hidden w-full flex items-center flex-col h-[132px] bg-[#313131] rounded-b-xl shadow-navShadow">
+                  <div className="flex mt-[7px] max-w-[334px] min-w-[334px] items-center justify-between px-3 bg-[#4B4B4B] shadow-sm h-[44px] rounded-xl">
+                    <p className="text-white flex items-center">
+                      <span className="text-xs ">A</span>A
+                      <span className="ml-1 text-[#8E8E92]">
+                        <PiPuzzlePieceFill />
+                      </span>
+                    </p>
+                    <div className="flex items-center mr-7">
+                      <span className="text-[#ABABB0] mr-1">
+                        <BiSolidLockAlt />
+                      </span>
+                      <h1 className="text-base">teste.com</h1>
+                    </div>
+                    <button
+                      onClick={() => setIsModalOpen(false)}
+                      className="hover:brightness-50 text-2xl"
+                    >
+                      <IoIosClose />
+                    </button>
+                  </div>
+                  <div className="flex flex-row justify-around w-full mt-3">
+                    <button className="text-[#5A91F7] hover:brightness-75 text-xl">
+                      <IoIosArrowBack />
+                    </button>
+                    <button className="text-[#5A91F7] hover:brightness-75 text-xl">
+                      <IoIosArrowForward />
+                    </button>
+                    <button className="text-[#5A91F7] hover:brightness-75 text-xl">
+                      <IoShareOutline />
+                    </button>
+                    <button className="text-[#5A91F7] hover:brightness-75 text-xl">
+                      <HiOutlineBookOpen />
+                    </button>
+                    <button className="text-[#5A91F7] hover:brightness-75 text-xl">
+                      <FiCopy />
+                    </button>
+                  </div>
+                  <div className="mt-7 bg-white w-[134px] h-1 rounded-full"></div>
+                </header>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
   );
 }
