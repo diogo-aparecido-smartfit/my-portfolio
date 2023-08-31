8736da7c9c3ed37bb5518a070b610c2a5de2d4e3
DiogoAMV <diogo.amv19@gmail.com>
2023-08-31 15:01:44 -0300

    unsed code removed

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,14 +1,5 @@
-import { GiBrazilFlag } from "react-icons/gi";
-import { FiHome } from "react-icons/fi";
-import {
-  LiaFlagUsaSolid,
-  LiaGraduationCapSolid,
-  LiaPhoneSolid,
-} from "react-icons/lia";
-import { MdOutlineWorkOutline } from "react-icons/md";
-import { LanguageContext } from "../App";
-import { useContext, useEffect, useRef, useState } from "react";
-import { BsGrid, BsMoon, BsSun } from "react-icons/bs";
+import { useRef } from "react";
+import { BsGrid } from "react-icons/bs";
 import { useKBar } from "kbar";
 import { BiCommand } from "react-icons/bi";
 
@@ -18,59 +9,10 @@ interface NavBarProps {
   darkMode: boolean;
 }
 
-export default function NavBar({
-  toggleDarkMode,
-  darkMode,
-  handleChangeLanguage,
-}: NavBarProps) {
-  const [isNavbarOpen, setIsNavbarOpen] = useState(true);
-  const language = useContext(LanguageContext);
+export default function NavBar({ darkMode }: NavBarProps) {
   const navbarRef = useRef<HTMLDivElement | null>(null);
 
   const { query } = useKBar();
-
-  // closes the mobile navbar if the user clicks outside of it
-  useEffect(() => {
-    const handleClickOutside = (event: { target: any }) => {
-      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-        setIsNavbarOpen(true);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  // open/close the mobile navbar
-  const toggleNavbar = () => {
-    setIsNavbarOpen(!isNavbarOpen);
-  };
-
-  const data = [
-    {
-      home: "Início",
-      projects: "Projetos",
-      education: "Educação",
-      contact: "Contato",
-      lightTheme: "Claro",
-      darkTheme: "Escuro",
-      portuguese: "Português",
-      english: "Inglês",
-    },
-    {
-      home: "Home",
-      projects: "Projects",
-      education: "Education",
-      contact: "Contact",
-      lightTheme: "Light",
-      darkTheme: "Dark",
-      portuguese: "Portuguese",
-      english: "English",
-    },
-  ];
 
   return (
     <header>
@@ -101,101 +43,6 @@ export default function NavBar({
           <button onClick={query.toggle}>
             <BsGrid />
           </button>
-        </div>
-        <div
-          className={`${
-            isNavbarOpen ? "-bottom-full" : "bottom-0"
-          } flex flex-row transition-all duration-500 w-screen fixed h-72 pt-8 px-6 pb-16 rounded-t-2xl justify-center shadow-navShadow z-10 ${
-            darkMode
-              ? "bg-[#2a2a2a] text-[#fff] transition-all"
-              : "bg-white text-[#111010] transition-all"
-          } `}
-        >
-          <ul className="grid flex-row flex-wrap font-medium text-base gap-8 text-center grid-cols-3">
-            <li>
-              <a
-                className="flex flex-col text-center items-center justify-center"
-                href="#home"
-              >
-                <span className="text-xl">
-                  <FiHome />
-                </span>
-                {language === "pt-BR" ? data[0].home : data[1].home}
-              </a>
-            </li>
-            <li>
-              <a
-                className="flex flex-col text-center items-center justify-center"
-                href="#projects"
-              >
-                <span className="text-xl">
-                  <MdOutlineWorkOutline />
-                </span>
-                {language === "pt-BR" ? data[0].projects : data[1].projects}
-              </a>
-            </li>
-            <li>
-              <a
-                className="flex flex-col text-center items-center justify-center"
-                href="#education"
-              >
-                <span className="text-xl">
-                  <LiaGraduationCapSolid />
-                </span>
-                {language === "pt-BR" ? data[0].education : data[1].education}
-              </a>
-            </li>
-            <li>
-              <a
-                className="flex flex-col text-center items-center justify-center"
-                href="#contact"
-                onClick={toggleNavbar}
-              >
-                <span className="text-xl">
-                  <LiaPhoneSolid />
-                </span>
-                {language === "pt-BR" ? data[0].contact : data[1].contact}
-              </a>
-            </li>
-            <li>
-              <span className="flex flex-col text-center items-center justify-center">
-                <button
-                  className="hover:scale-125 text-xl transition-all"
-                  onClick={toggleDarkMode}
-                >
-                  {darkMode ? <BsMoon /> : <BsSun />}
-                </button>
-                {darkMode
-                  ? language === "pt-BR"
-                    ? data[0].darkTheme
-                    : data[1].darkTheme
-                  : language === "pt-BR"
-                  ? data[0].lightTheme
-                  : data[1].lightTheme}
-              </span>
-            </li>
-            <li>
-              <span className="flex flex-col text-center items-center justify-center">
-                <button
-                  className="hover:scale-125 transition-all"
-                  onClick={handleChangeLanguage}
-                >
-                  {language === "en-US" ? (
-                    <GiBrazilFlag />
-                  ) : (
-                    <LiaFlagUsaSolid />
-                  )}
-                </button>
-                {language === "en-US"
-                  ? language === "en-US"
-                    ? data[1].portuguese
-                    : data[0].portuguese
-                  : language === "en-US"
-                  ? data[1].english
-                  : data[0].english}
-              </span>
-            </li>
-          </ul>
         </div>
       </nav>
     </header>
