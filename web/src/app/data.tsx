38622721351599409420d0450f09f935577c479e
DiogoAMV <diogo.amv19@gmail.com>
2023-11-03 21:04:38 -0300

    new data added

--- web/src/app/data.tsx
+++ web/src/app/data.tsx
@@ -1,5 +1,7 @@
 import renerForce from "../../public/experience-images/logo/rener-force.svg";
 import uniube from "../../public/education-images/uniube.png";
+import jstack from "../../public/education-images/jstack.png";
+import balta from "../../public/education-images/balta.jpg";
 import { BiLogoTypescript, BiLogoReact } from "react-icons/bi";
 import { FaNodeJs } from "react-icons/fa";
 import { SiDotnet, SiMicrosoftsqlserver, SiNextdotjs } from "react-icons/si";
@@ -17,8 +19,26 @@ export const education = [
   {
     id: 1,
     title: "Análise e Desenvolvimento de Sistemas ・ UNIUBE",
-    subtitle: "2023 - em andamento",
+    subtitle: "fev 2023 - em andamento.",
     image: uniube,
+  },
+  {
+    id: 2,
+    title: "Curso Full-Stack ・ JStack",
+    subtitle: "jun 2022 - em andamento.",
+    image: jstack,
+  },
+  {
+    id: 3,
+    title: "Fundamentos do C# ・ Balta.io",
+    subtitle: "jun 2023 - jul 2023.",
+    image: jstack,
+  },
+  {
+    id: 4,
+    title: "Ignite ReactJS ・ RocketSeat",
+    subtitle: "nov 2021 - abr 2022.",
+    image: jstack,
   },
 ];
 
