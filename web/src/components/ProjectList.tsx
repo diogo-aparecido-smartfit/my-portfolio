60133d5e9a52d99ad7b38a3c8c49a993cdba2645
DiogoAMV <diogo.amv19@gmail.com>
2023-11-29 14:54:34 -0300

    feat: responsive styles fixed

--- web/src/components/ProjectList.tsx
+++ web/src/components/ProjectList.tsx
@@ -30,15 +30,16 @@ export default function ProjectList({ projectsPage, title }: ProjectListProps) {
   return (
     <div className="flex flex-col">
       <div className="flex flex-row justify-between items-center mb-4">
-        <h1 className="text-xl sm:text-2xl font-semibold  text-zinc-200">
+        <h1 className="text-lg sm:text-2xl font-semibold  text-zinc-200">
           {title}
         </h1>
         {projectsPage ? null : (
           <Link
             href="/experience"
-            className="flex items-center rounded-lg bg-darkBg border-[1px] border-darkBorder px-5 py-1 w-fit gap-2 hover:brightness-150 transition-all justify-between duration-300"
+            className="flex items-center rounded-lg bg-darkBg border-[1px] border-darkBorder px-2 py-1 sm:px-5 sm:py-1 w-fit gap-1 sm:gap-2 hover:brightness-150 transition-all justify-between duration-300"
           >
-            Ver todos <IoIosArrowForward />
+            <span className="hidden sm:flex">Ver todos</span>{" "}
+            <IoIosArrowForward />
           </Link>
         )}
       </div>
