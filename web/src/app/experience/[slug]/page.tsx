26455d1153de414c076eb31048704b153ee26ef3
DiogoAMV <diogo.amv19@gmail.com>
2023-11-29 14:56:23 -0300

    feat: description of project removed & recommended projects added on bottom of page

--- web/src/app/experience/[slug]/page.tsx
+++ web/src/app/experience/[slug]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { projects } from "@/app/data";
+import ProjectList from "@/components/ProjectList";
 import Text from "@/components/Text";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
@@ -17,7 +18,6 @@ interface ProjectProps {
   jobType: string;
   github: string;
   deploy: string;
-  description: string;
   overview: JSX.Element;
   specifications: string[];
   detailsProjectImages?: StaticImageData[];
@@ -78,89 +78,88 @@ export default function Post() {
                 {post.title}
               </h1>
             </div>
+          </header>
+          {post.detailsProjectImages ? (
+            <ul className="grid grid-cols-1 sm:grid-cols-2 w-full gap-2 mt-6 ">
+              {post.detailsProjectImages.map((image, i) => (
+                <button onClick={() => handleShowImage(image)}>
+                  <Image
+                    key={i}
+                    alt="Imagem do projeto"
+                    src={image}
+                    className="w-full h-full rounded-xl aspect-video border-[1px] border-zinc-900 hover:border-zinc-700 transition-all duration-300"
+                  />
+                </button>
+              ))}
+            </ul>
+          ) : (
+            <Image
+              alt="Imagem do projeto"
+              src={post.image}
+              className="rounded-xl w-full h-full mt-6"
+            />
+          )}
 
-            {post.detailsProjectImages ? (
-              <ul className="grid grid-cols-1 sm:grid-cols-2 w-full gap-2 mt-6 ">
-                {post.detailsProjectImages.map((image, i) => (
-                  <button onClick={() => handleShowImage(image)}>
-                    <Image
-                      key={i}
-                      alt="Imagem do projeto"
-                      src={image}
-                      className="w-full h-full rounded-xl aspect-video border-[1px] border-zinc-900 hover:border-zinc-700 transition-all duration-300"
-                    />
-                  </button>
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-2 mt-6 w-full">
+            <li className="flex flex-col gap-1 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
+              <h2 className="text-zinc-500">🕒 Período:</h2>
+              <p className="text-white font-semibold">{post.period}</p>
+            </li>
+            <li className="flex flex-col gap-1 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
+              <h2 className="text-zinc-500">🖥️ Tipo de projeto:</h2>
+              <p className="text-white font-semibold">{post.projectType}</p>
+            </li>
+            <li className="flex flex-col gap-1 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
+              <h2 className="text-zinc-500">👔 Cargo:</h2>
+              <p className="text-white font-semibold">{post.jobType}</p>
+            </li>
+          </ul>
+
+          <ul className="flex flex-col sm:flex-row gap-2 mt-6 w-full text-base sm:text-lg">
+            <li className="flex flex-col gap-3 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
+              <h2 className="font-semibold text-xl">Visão geral</h2>
+              <div className="flex flex-col text-zinc-500 gap-3 overview">
+                {post.overview}
+              </div>
+              <div className="flex flex-col md:flex-row w-full gap-3 md:gap-1">
+                {post.github && (
+                  <a
+                    href={post.github}
+                    target="_blank"
+                    className="flex items-center rounded-xl bg-transparent border-[1px] border-darkBorder p-3 w-full hover:border-zinc-400  justify-center gap-4   transition-all duration-200  origin-customTransform text-sm xl:text-base text-white"
+                  >
+                    <FiGithub /> GitHub
+                  </a>
+                )}
+                {post.deploy && (
+                  <a
+                    target="_blank"
+                    href={post.deploy}
+                    className="flex items-center rounded-xl bg-zinc-800 border-[1px] border-darkBorder p-3 w-full hover:bg-white hover:text-zinc-900 hover:border-zinc-800  justify-center gap-4   transition-all duration-200 origin-customTransform text-sm xl:text-base"
+                  >
+                    <FiExternalLink /> Ver Demo
+                  </a>
+                )}
+              </div>
+            </li>
+            <li className="flex flex-col p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
+              <h2 className="font-semibold text-xl">Especificações</h2>
+              <ul className="text-zinc-500 mt-3">
+                {post.specifications.map((item, i) => (
+                  <li className="" key={i}>
+                    {item}
+                    {i === post.specifications.length - 1 ? null : (
+                      <div className="h-[1px] bg-zinc-800 w-full my-3"></div>
+                    )}
+                  </li>
                 ))}
               </ul>
-            ) : (
-              <Image
-                alt="Imagem do projeto"
-                src={post.image}
-                className="rounded-xl w-full h-full mt-6"
-              />
-            )}
+            </li>
+          </ul>
 
-            <ul className="grid grid-cols-1 sm:grid-cols-3 gap-2 mt-6 w-full">
-              <li className="flex flex-col gap-1 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
-                <h2 className="text-zinc-500">🕒 Período:</h2>
-                <p className="text-white font-semibold">{post.period}</p>
-              </li>
-              <li className="flex flex-col gap-1 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
-                <h2 className="text-zinc-500">🖥️ Tipo de projeto:</h2>
-                <p className="text-white font-semibold">{post.projectType}</p>
-              </li>
-              <li className="flex flex-col gap-1 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
-                <h2 className="text-zinc-500">👔 Cargo:</h2>
-                <p className="text-white font-semibold">{post.jobType}</p>
-              </li>
-            </ul>
-
-            <ul className="flex flex-col sm:flex-row gap-2 mt-6 w-full text-base sm:text-lg">
-              <li className="flex flex-col gap-3 p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
-                <h2 className="font-semibold text-xl">Visão geral</h2>
-                <div className="flex flex-col text-zinc-500 gap-3 overview">
-                  {post.overview}
-                </div>
-                <div className="flex flex-col md:flex-row w-full gap-3 md:gap-1">
-                  {post.github && (
-                    <a
-                      href={post.github}
-                      target="_blank"
-                      className="flex items-center rounded-xl bg-transparent border-[1px] border-darkBorder p-3 w-full hover:border-zinc-400  justify-center gap-4   transition-all duration-200  origin-customTransform text-sm xl:text-base text-white"
-                    >
-                      <FiGithub /> GitHub
-                    </a>
-                  )}
-                  {post.deploy && (
-                    <a
-                      target="_blank"
-                      href={post.deploy}
-                      className="flex items-center rounded-xl bg-zinc-800 border-[1px] border-darkBorder p-3 w-full hover:bg-white hover:text-zinc-900 hover:border-zinc-800  justify-center gap-4   transition-all duration-200 origin-customTransform text-sm xl:text-base"
-                    >
-                      <FiExternalLink /> Ver Demo
-                    </a>
-                  )}
-                </div>
-              </li>
-              <li className="flex flex-col p-4 rounded-xl bg-zinc-900 w-full border-[1px] border-zinc-800">
-                <h2 className="font-semibold text-xl">Especificações</h2>
-                <ul className="text-zinc-500 mt-3">
-                  {post.specifications.map((item, i) => (
-                    <li className="" key={i}>
-                      {item}
-                      {i === post.specifications.length - 1 ? null : (
-                        <div className="h-[1px] bg-zinc-800 w-full my-3"></div>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            </ul>
-          </header>
-
-          <section className="flex flex-col my-6 w-full rounded-xl bg-zinc-900 p-6 text-zinc-500 border-[1px] border-zinc-800">
-            <Text type="paragraph">{post.description}</Text>
-          </section>
+          <div className="flex mt-24">
+            <ProjectList title="Você também pode gostar" />
+          </div>
         </article>
       </div>
     )
